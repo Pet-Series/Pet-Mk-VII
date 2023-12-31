25f03e5957a1d5281b037cf379a8f082fd208238
kullken <karl.viktor.kull@gmail.com>
2023-12-31 17:04:05 +0100

    feat: Add iteration loop in RRT

--- pet_mk_vii_planner/src/rrt.cpp
+++ pet_mk_vii_planner/src/rrt.cpp
@@ -6,25 +6,25 @@ namespace pet::rrt
 std::optional<std::vector<Node>> search(const Goal &goal, Graph &tree,
                                         const SearchContext &context)
 {
-    const auto sampledState = goal.sampleState();
-
-    const Node &parentNode = tree.findClosest(sampledState);
-
-    /// TODO: Add steering function.
-    ControlInput controlInput{};
-
-    const Node &newNode = tree.addNode(sampledState, controlInput, parentNode);
-
-    /// TODO: Add check if result from steering is in goal.
-    const bool goalReached = true;
-    if (goalReached)
+    for (int i = 0; i < context.m_iterations; ++i)
     {
-        return tree.getPathFromRoot(newNode);
+        const auto sampledState = goal.sampleState();
+
+        const Node &parentNode = tree.findClosest(sampledState);
+
+        /// TODO: Add steering function.
+        ControlInput controlInput{};
+
+        const Node &newNode = tree.addNode(sampledState, controlInput, parentNode);
+
+        /// TODO: Add check if result from steering is in goal.
+        const bool goalReached = true;
+        if (goalReached)
+        {
+            return tree.getPathFromRoot(newNode);
+        }
     }
-    else
-    {
-        return {};
-    }
+    return {};
 }
 
 } // namespace pet::rrt
