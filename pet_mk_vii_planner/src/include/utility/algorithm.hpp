89c02a3afbc4e53c511133211ec38b8cf44d5118
kullken <karl.viktor.kull@gmail.com>
2024-03-09 15:14:32 +0100

    feat: Implement min_element_transform()

--- pet_mk_vii_planner/src/include/utility/algorithm.hpp
+++ pet_mk_vii_planner/src/include/utility/algorithm.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 namespace pet
 {
 namespace util
@@ -20,6 +22,26 @@ constexpr void adjacent_for_each(ForwardIt first, ForwardIt last, BinaryFunction
     {
         function(*first, *next);
     }
+}
+
+template <typename ForwardIt, typename UnaryFunction>
+[[nodiscard]] constexpr ForwardIt min_element_transform(ForwardIt first, ForwardIt last,
+                                                        UnaryFunction transform)
+{
+    using ResultType = std::invoke_result_t<UnaryFunction>;
+
+    ForwardIt  minIterator = last;
+    ResultType minValue;
+    for (; first != last; ++first)
+    {
+        const auto value = transform(*first);
+        if (value < minValue)
+        {
+            minIterator = first;
+            minValue    = value;
+        }
+    }
+    return minIterator;
 }
 
 } // namespace util
